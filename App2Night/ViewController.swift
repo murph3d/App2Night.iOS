55d66cb8b4ba8e5c2c69c9ad7c71a259fe3e4b47
Robin Niebergall <r-niebergall@gmx.de>
2016-10-22 19:24:46 +0200

    Initial Commit

---
+++ App2Night/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App2Night
+//
+//  Created by Robin Niebergall on 22.10.16.
+//  Copyright © 2016 DHBW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

99ec9638b2252de351168e79584b910a30eafb5b
Robin Niebergall <r-niebergall@gmx.de>
2016-10-29 19:25:32 +0200

    Refactored SwaggerCommunication, added Properties class, added AppIcon

---
+++ App2Night/Properties.swift
@@ -0,0 +1,21 @@
+//
+//  Properties.swift
+//  App2Night
+//
+//  Created by Robin Niebergall on 29.10.16.
+//  Copyright © 2016 DHBW. All rights reserved.
+//
+
+import Foundation
+
+public class Properties {
+    
+    // MARK: Global variables
+    public static var baseUrl = "http://app2nightapi.azurewebsites.net/api/"
+    public static var partyUrl = baseUrl + "Party"
+    public static var userUrl = baseUrl + "User"
+    public static var valuesUrl = baseUrl + "Values"
+    
+    
+}
+
--- App2Night/SwaggerCommunication.swift
+++ App2Night/SwaggerCommunication.swift
@@ -12,120 +12,123 @@ import Alamofire
 public class SwaggerCommunication {
     
     // MARK: Variables
-    private static let baseUrl = "http://app2nightapi.azurewebsites.net/api/"
-    private static let partyUrl = baseUrl + "Party"
-    private static var parties: [Party]? = [Party]()
+    public static var parties: [Party] = [Party]()
     
-    
-    // MARK: getPartiesArray()
-    public static func getPartiesArray() -> [Party]? {
-        return parties
+    // MARK: Alamofire requests
+    public static func getParty(completionHandler: @escaping ([Party]) -> ()) {
+        Alamofire.request(Properties.partyUrl).validate().responseJSON { response in
+            // Debug messages from response
+            debugPrint(response.request)    // original URL request
+            debugPrint(response.response)   // HTTP URL response
+            debugPrint(response.data)   // server data
+            debugPrint(response.result) // result of response serialization
+            debugPrint(response.result.value)   // value of the response result
+            
+            switch response.result {
+            case .success:
+                debugPrint("Validation Successful")
+                parseParty(pResponseData: response.result.value)
+                // printArray()
+            case .failure(let error):
+                debugPrint(error)
+            }
+            
+            DispatchQueue.main.async(execute: { () -> Void in
+                completionHandler(self.parties)
+            })
+            
+        }.resume()
     }
     
-    // MARK: getParty()
-    public static func getParty() {
-        Alamofire.request(self.partyUrl).responseJSON { response in
-            print(response.request)  // original URL request
-            print(response.response) // HTTP URL response
-            print(response.data)     // server data
-            print(response.result)   // result of response serialization
+    public static func postParty(pDictionary: [String: Any]) {
+        // post request
+        var postUrl = URLRequest(url: URL(string: Properties.partyUrl)!)
+        postUrl.httpMethod = "POST"
+        postUrl.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // try json serialization
+        postUrl.httpBody = try! JSONSerialization.data(withJSONObject: pDictionary)
+        
+        Alamofire.request(postUrl).validate().responseJSON { response in
+            // Debug messages from response
+            debugPrint(response.request)    // original URL request
+            debugPrint(response.response)   // HTTP URL response
+            debugPrint(response.data)   // server data
+            debugPrint(response.result) // result of response serialization
+            debugPrint(response.result.value)   // value of the response result
             
-            self.parseParty(pResponseData: response.result.value as! [[String: AnyObject]])
-            
+            switch response.result {
+            case .success:
+                debugPrint("Validation Successful")
+            case .failure(let error):
+                debugPrint(error)
+                if let responseData = response.data, let responseString = String(data: responseData, encoding: .utf8) {
+                    print(responseString)
+                }
+            }
         }
- 
     }
     
-    // MARK: parseParty()
-    private static func parseParty(pResponseData: [[String: AnyObject]]) {
-        for Dictionary in pResponseData {
+    // MARK: parse response
+    private static func parseParty(pResponseData: Any?) {
+        let responseData = pResponseData as! [[String: AnyObject]]
+        
+        for Dictionary in responseData {
+            // root model
             let partyDictionary = Dictionary as NSDictionary
             let party = Party(pDictionary: partyDictionary)
             
+            // host model
             let hostDictionary = Dictionary["host"] as! NSDictionary
             let host = Host(pDictionary: hostDictionary)
             party.setHost(pHost: host)
             
-            /*
-             let hostLocationDictionary = Dictionary["host"]?["location"] as! NSDictionary
-             let hostLocation = Location(fromDictionary: hostLocationDictionary)
-             party.host.location = hostLocation
-             */
+            // host.location model
+            let hostLocationDictionary = Dictionary["host"]!["location"] as! NSDictionary
+            let hostLocation = Location(pDictionary: hostLocationDictionary)
+            party.getHost().setLocation(pLocation: hostLocation)
             
+            // location model
             let locationDictionary = Dictionary["location"] as! NSDictionary
             let location = Location(pDictionary: locationDictionary)
             party.setLocation(pLocation: location)
             
-            parties?.append(party)
+            // append to array of parties
+            self.parties.append(party)
         }
-        self.printArray()
+        
+        debugPrint("Parsed response data.")
     }
     
-    // MARK: postParty()
-    public static func postParty() {
-        let testParty = [
-            "partyName": "iOS Post Test 2",
-            "partyDate": "2016-10-25T17:12:22.865Z",
-            "musicGenre": 0,
-            "location": [
-                "countryName": "string",
-                "cityName": "string",
-                "streetName": "string",
-                "houseNumber": 0,
-                "houseNumberAdditional": "string",
-                "zipcode": 0,
-                "latitude": 0,
-                "longitude": 0
-            ],
-            "partyType": 0,
-            "description": "string"
-            ] as [String: Any]
-        
-        let postUrl = URL(string: partyUrl)
-        var request = URLRequest(url: postUrl!)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try! JSONSerialization.data(withJSONObject: testParty)
-        
-        Alamofire.request(request)
-            .responseJSON { response in
-                switch response.result {
-                case .failure(let error):
-                    print(error)
-                    if let data = response.data, let responseString = String(data: data, encoding: .utf8) {
-                        print(responseString)
-                    }
-                case .success(let responseObject):
-                    print(responseObject)
-                }
+    // MARK: Debug functions
+    public static func printArray() {
+        debugPrint("Printing array content..")
+        for Party in self.parties {
+            let partyVariable = Party.getPartyName()
+            debugPrint(partyVariable)
         }
     }
     
-    // MARK: printArray()
-    private static func printArray() {
-        for Party in self.parties! {
-            let tmp = Party.getPartyName()
-            print(tmp)
-        }
+    public static func postTestParty() {
+        let testParty = Party()
+        let testLocation = Location()
+        testParty.setLocation(pLocation: testLocation)
+        testParty.setPartyName(pPartyName: "iOS dummy party")
+        testParty.setPartyDate(pPartyDate: "2016-12-24T20:00:00.000Z")
+        testParty.setMusicGenre(pMusicGenre: 0)
+        testParty.setPartyType(pPartyType: 0)
+        testParty.setDescription(pDescription: "string")
+        testLocation.setCountryName(pCountryName: "string")
+        testLocation.setCityName(pCityName: "string")
+        testLocation.setStreetName(pStreetName: "string")
+        testLocation.setHouseNumber(pHouseNumber: 0)
+        testLocation.setHouseNumberAdditional(pHouseNumberAdditional: "string")
+        testLocation.setZipcode(pZipcode: 0)
+        testLocation.setLatitude(pLatitude: 0)
+        testLocation.setLongitude(pLongitude: 0)
+        
+        postParty(pDictionary: testParty.toDictionary())
     }
     
 }
-
-/*
-// MARK: getParty()
-public static func getParty(completionHandler:@escaping ([Party]) -> ()) {
-    Alamofire.request(self.partyUrl).responseJSON { response in
-        print(response.request)  // original URL request
-        print(response.response) // HTTP URL response
-        print(response.data)     // server data
-        print(response.result)   // result of response serialization
-        
-        self.parseParty(pResponseData: response.result.value as! [[String: AnyObject]])
-        
-        DispatchQueue.main.async(execute: { () -> Void in
-            completionHandler(parties!)
-        })
-        }.resume()
-}
-*/
 

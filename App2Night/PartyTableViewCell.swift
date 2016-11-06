98444169d4bd32bfcda57cea0f2d76a0a2cf0b46
Robin Niebergall <r-niebergall@gmx.de>
2016-11-06 02:24:58 +0100

    Updated project settings, clean-up, updated Core Data model

--- App2Night/PartyTableViewCell.swift
+++ App2Night/PartyTableViewCell.swift
@@ -15,16 +15,16 @@ class PartyTableViewCell: UITableViewCell {
 	@IBOutlet weak var PartyLocation: UILabel!
 	@IBOutlet weak var Distance: UILabel!
 	
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		// Initialization code
+	}
+	
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+		
+		// Configure the view for the selected state
+	}
+	
 }
--- App2Night/ViewController.swift
+++ App2Night/ViewController.swift
@@ -2,17 +2,16 @@
 //  ViewController.swift
 //  App2Night
 //
-//  Created by Robin Niebergall on 01.11.16.
+//  Created by Robin Niebergall on 06.11.16.
 //  Copyright © 2016 DHBW. All rights reserved.
 //
 
 import UIKit
 
-class LoginController: UIViewController {
+class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
 		view.backgroundColor = .white
 	}
 	
